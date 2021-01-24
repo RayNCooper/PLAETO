5e6ea92e1363c475127b304deb83f80acf98a8ef
crunch <dennis.reinkober@gmx.de>
2021-01-24 21:19:04 +0100

    feat: added logging to socket view, improved socket worker script

--- plaeto-backend/plaeto_project/socketsapp/views.py
+++ plaeto-backend/plaeto_project/socketsapp/views.py
@@ -1,18 +1,36 @@
-async_mode = None
-
-import os
 import socketio
+import os
+import logging
+import coloredlogs
+
+""" Initialize Logger
+
+    Available Logging Levels:
+    - debug (higher level needed for display, see 'level' attribute in coloredlogs.install)
+    - info
+    - warning
+    - error
+    - critical
+"""
+logger = logging.getLogger(__name__)
+
+""" Set Logger config such as format or color """
+coloredlogs.install(level='INFO', logger=logger,
+                    fmt='%(asctime)s %(levelname)s \033[32mSERVER:\x1b[0m %(message)s', datefmt='%H:%M:%S')
 
 sio = socketio.Server(async_mode="eventlet", cors_allowed_origins='*')
 
+
 @sio.event
 def transmit(sid, data):
-    print('transmitted data: ', data)
+    logger.info('Transmitted data ' + data + ' to Client with ID ' + sio.id)
+
 
 @sio.event
 def connect(sid, environ):
-    print('connect', sid)
+    logger.critical('Connected to Client with ID ' + sid)
+
 
 @sio.event
 def disconnect(sid):
-    print('disconnect', sid)
+    logger.info('Server: Disconnected from Client with ID ' + id)
