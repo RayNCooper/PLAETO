febc768d4207e49cb48cd0c2d30ab0e59ef34496
crunch <dennis.reinkober@gmx.de>
2021-01-25 03:27:33 +0100

    feat: implemented curve relay listener in vue

--- plaeto-frontend/src/main.ts
+++ plaeto-frontend/src/main.ts
@@ -2,11 +2,24 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueSocketIO from "vue-socket.io";
 
 Vue.config.productionTip = false;
+
+Vue.use(
+  new VueSocketIO({
+    debug: false,
+    connection: "http://localhost:5000",
+    vuex: {
+      store,
+      actionPrefix: "SOCKET_",
+      mutationPrefix: "SOCKET_",
+    },
+  })
+);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
