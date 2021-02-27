cdc49fdb071d0884adeceae80c4816f4eac09cf7
crunch <dennis.reinkober@gmx.de>
2021-02-27 22:29:21 +0100

    feat: added ui for inputting and displaying metadata

---
+++ plaeto-frontend/src/components/Stream/ProjectPanel/ProjectPanel.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-row no-gutters>
+    <v-col>
+      <v-card class="pa-2 grey lighten-2" rounded="lg" outlined>
+        <v-card class="mt-3 mb-3 ml-2 mr-2 pa-2 blue-grey lighten-3">
+          <v-card-title class="white--text">Metadata</v-card-title>
+          <v-card flat class="pl-3 pr-3 pt-5 pb- white">
+            <v-text-field
+              :value="selectedTraceProject.title"
+              background-color="grey lighten-4"
+              dense
+              label="Title"
+              placeholder="Office Energy Record"
+              outlined
+              :readonly="inPlaybackMode"
+            ></v-text-field>
+            <v-text-field
+              :value="selectedTraceProject.metadata['country']"
+              background-color="grey lighten-4"
+              dense
+              label="Country"
+              placeholder="Germany"
+              outlined
+              :readonly="inPlaybackMode"
+              append-icon="mdi-flag"
+            ></v-text-field>
+            <v-text-field
+              :value="selectedTraceProject.metadata['city']"
+              background-color="grey lighten-4"
+              dense
+              label="City"
+              placeholder="Essen, Ruhr"
+              outlined
+              :readonly="inPlaybackMode"
+              append-icon="mdi-city"
+            ></v-text-field>
+            <v-text-field
+              :value="selectedTraceProject.metadata['weather']"
+              background-color="grey lighten-4"
+              dense
+              label="Weather"
+              placeholder="Rainy"
+              outlined
+              :readonly="inPlaybackMode"
+              append-icon="mdi-cloud"
+            ></v-text-field>
+            <v-text-field
+              :value="lux"
+              background-color="grey lighten-4"
+              dense
+              label="Lux"
+              placeholder="1200"
+              outlined
+              :readonly="inPlaybackMode"
+              append-icon="mdi-lightbulb"
+            ></v-text-field>
+            <v-text-field
+              :value="selectedTraceProject.metadata['environment']"
+              background-color="grey lighten-4"
+              dense
+              label="Environment"
+              placeholder="Urban"
+              outlined
+              :readonly="inPlaybackMode"
+              append-icon="mdi-domain"
+            ></v-text-field>
+          </v-card>
+        </v-card>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from "vue-property-decorator";
+import { mapGetters, mapMutations } from "vuex";
+
+@Component({
+  computed: mapGetters(["inPlaybackMode", "selectedTraceProject"]),
+  methods: mapMutations([]),
+  components: {}
+})
+export default class ProjectPanel extends Vue {
+  get lux() {
+    if (this.$store.getters.inPlaybackMode)
+      return this.$store.getters.selectedTraceProject.metadata.lux;
+    return "";
+  }
+}
+</script>
+
+<style scoped></style>
--- plaeto-frontend/src/views/Content/Stream.vue
+++ plaeto-frontend/src/views/Content/Stream.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <three-column-content-view>
-      <template v-slot:leftColumn><traces-panel></traces-panel></template>
+      <template v-slot:leftColumn>
+        <traces-panel></traces-panel>
+      </template>
+
       <template v-slot:centerColumn>
         <stream-panel></stream-panel>
       </template>
-      <template v-slot:rightColumn> </template>
+
+      <template v-slot:rightColumn>
+        <project-panel></project-panel>
+      </template>
     </three-column-content-view>
     <socket-status-snackbar></socket-status-snackbar>
   </div>
@@ -16,12 +22,14 @@ import ThreeColumnContentView from "@/views/Layout/ThreeColumnContentView.vue";
 import { Component, Vue } from "vue-property-decorator";
 import StreamPanel from "@/components/Stream/StreamPanel/StreamPanel.vue";
 import TracesPanel from "@/components/Stream/TracesPanel/TracesPanel.vue";
+import ProjectPanel from "@/components/Stream/ProjectPanel/ProjectPanel.vue";
 import SocketStatusSnackbar from "@/components/Stream/SocketStatus/SocketStatusSnackbar.vue";
 
 @Component({
   components: {
     StreamPanel,
     TracesPanel,
+    ProjectPanel,
     ThreeColumnContentView,
     SocketStatusSnackbar
   }
